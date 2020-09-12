e71afafb19e7f7b272d786b7955d2f67175c38b7
Farhad Bahrehmand <43734894+farhadbahrehmandhenry@users.noreply.github.com>
2020-09-11 21:47:44 -0700

    deploy

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var path = require('path');
 var app = express()
 var port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+app.use('/asset', express.static(__dirname + '/asset'));
+app.use('/styles', express.static(__dirname + '/styles'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/javascripts', express.static(__dirname + '/javascripts'));
+
+console.log(__dirname)
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
